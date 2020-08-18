817c4279b40b4b9f1f61d54c004563fc96039517
ben <ben.paul.ryan.packer@gmail.com>
2020-08-18 14:44:06 -0500

    Update graphql.js

--- server/api/graphql.js
+++ server/api/graphql.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server-express";
+import { db } from '../lib/db';
 
 const typeDefs = gql`
   type User {
@@ -10,12 +11,34 @@ const typeDefs = gql`
   type Query {
     me: User
   }
+
+  type Mutation {
+    resetPassword(email: String!): Boolean!
+  }
 `;
 
 const resolvers = {
   Query: {
     me: (parent, args, { user }) => user,
   },
+  Mutation: {
+    resetPassword: async (parent, args, { user }) => {
+      const { email } = args;
+      const user = await db('user').where({ email }).first('*');
+      
+      if (user !== undefined) {
+        const token = Math.random().toString().split('.')[1]
+        await db('token').insert({ user_id: user.id, token: token });
+        await sendEmail({ token, email });
+        /**
+         * Hey! Please reset your password at url.com/reset/${token}
+         */
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 };
 
 export const apolloServer = new ApolloServer({
